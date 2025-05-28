c4e9591c1255f96e0282ec18ece458e8002e54be
maryam najafi <maryam.najafi733@.com>
2025-05-28 13:24:30 +0330

    admin and user blueprint added

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import  Flask
 from  blueprints.general import app as general
+from  blueprints.admin import app as admin
+from  blueprints.user import app as user
+
+
 app=Flask(__name__)
 app.register_blueprint(general)
+app.register_blueprint(admin)
+app.register_blueprint(user)
+
 
 if __name__=='__main__':
     app.run()
---
+++ blueprints/admin.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+app=Blueprint("admin",__name__)
+
+@app.route('/admin')
+def admin():
+    return 'this is admin page'
