dea55bfd0bb1c6f800b9b4904c33f59d50860492
maryam najafi <maryam.najafi733@.com>
2025-06-02 20:51:56 +0330

    products page for admin panel

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,23 +1,56 @@
-from flask import Blueprint,render_template,request,session,redirect,abort
+from flask import Blueprint, render_template, request, session, redirect, abort
 import config
-app=Blueprint("admin",__name__)
+from extentions import db
+from models.product import Product
 
-@app.route('/admin/login',methods=['POST','GET'])
+app = Blueprint("admin", __name__)
+
+
+@app.before_request
+def before_request():
+    if session.get('admin_login', None) == None and request.endpoint != 'admin.login':
+        abort(403)
+
+
+@app.route('/admin/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        username=request.form.get('username',None)
-        password= request.form.get('password', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
 
-        if username== config.ADMIN_USERNAME and  password== config.ADMIN_PASSWORD:
-            session['admin_login']=username
-            return redirect('/admin/dasboard')
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD:
+            session['admin_login'] = username
+            return redirect('/admin/dashboard')
         else:
-            return  redirect('/admin/login')
+            return redirect('/admin/login')
     else:
-     return render_template("admin/login.html")
+        return render_template("admin/login.html")
 
-@app.route('/admin/dasboard',methods=['GET'])
+
+@app.route('/admin/dashboard', methods=['GET'])
 def dashboard():
-    if session.get('admin_login',None)==None:
-        abort(403)
-    return 'dashboard'
+    return render_template("admin/dashboard.html")
+
+
+@app.route("/admin/dashboard/products", methods=["GET", "POST"])
+def products():
+    if request.method == "GET":
+        products=Product.query.all()
+        return render_template("admin/products.html",products=products)
+    else:
+        name = request.form.get('name', None)
+        description = request.form.get('description', None)
+        price = request.form.get('price', None)
+        active = request.form.get('active', None)
+
+        p = Product(name=name, description=description, price=price)
+
+        if active is None:
+            p.active = 0
+        else:
+            p.active = 1
+
+        db.session.add(p)
+        db.session.commit()
+
+        return "done"
--- models/product.py
+++ models/product.py
@@ -5,8 +5,10 @@ class Product(db.Model):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False ,index=true)
-    discription = Column(String(11), nullable=False, index=true)
+    description = Column(String(11), nullable=False, index=true)
     price=Column(Integer, nullable=False,index=true)
+    active=Column(Integer, nullable=False,index=true)
 
 
-    address = Column(String, nullable=False,index=true)A
+
+
