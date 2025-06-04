f47fae27653433888195c96116e205af44d8a8a8
maryam najafi <maryam.najafi733@.com>
2025-06-04 12:20:07 +0330

    product page adde

--- blueprints/general.py
+++ blueprints/general.py
@@ -2,12 +2,20 @@ from flask import Blueprint, render_template
 
 from models.product import Product
 
-app=Blueprint("general",__name__)
+app = Blueprint("general", __name__)
+
 
 @app.route('/')
 def main():
-    products=Product.query.all()
-    return render_template('main.html',products=products)
+    products = Product.query.filter(Product.active==1).all()
+    return render_template('main.html', products=products)
+
+
+@app.route('/product/<int:id>/<name>')
+def product(id, name):
+    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(Product.active==1).first_or_404()
+    return render_template('product.html', product=product)
+
 
 @app.route('/about')
 def about():
