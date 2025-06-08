f7e9528b797f188a64d7ddbdbe584702c2fa8fe9
maryam najafi <maryam.najafi733@.com>
2025-06-08 12:40:57 +0330

    cart and cart_items and payment model added

--- models/cart.py
+++ models/cart.py
@@ -4,6 +4,6 @@ from  extentions import db
 class Cart(db.Model):
     __tablename__ = "carts"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeinKey('users.id'),nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
     status= Column(String, default='pending')
     user=db.relationship('User', backref='carts')
--- models/payment.py
+++ models/payment.py
@@ -4,6 +4,6 @@ from  extentions import db
 class Payment(db.Model):
     __tablename__ = "payments"
     id = Column(Integer, primary_key=True)
-    cart_id = Column(Integer, ForeinKey('carts.id'),nullable=False)
+    cart_id = Column(Integer, ForeignKey('carts.id'),nullable=False)
     status= Column(String, default='pending')
     cart=db.relationship('Carts', backref='payments')
